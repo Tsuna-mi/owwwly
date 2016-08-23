e6416f9d2cf22b93ce66c9e05318100d36f72a53
Ana Gamito <imaginarte3d@gmail.com>
2016-08-23 11:26:43 +0200

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name owwwlyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the owwwlyApp
+ */
+angular.module('owwwlyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name owwwlyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the owwwlyApp
+ */
+angular.module('owwwlyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
