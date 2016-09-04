c775c2e7ee885eadde754d7b24e831122dfcd7c9
Ana Gamito <imaginarte3d@gmail.com>
2016-09-04 17:20:46 +0200

    little change in boards

--- app/scripts/controllers/boards.js
+++ app/scripts/controllers/boards.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('owwwlyApp')
-	.controller('BoardsController',['$scope', '$http', function($scope, $http){
+	.controller('BoardsController', ['$scope', '$http', function ($scope, $http) {
 		$http.get('data/events.json')
-		.success(function(data){
+		.success(function (data) {
 			$scope.events = data;
 
 		});
